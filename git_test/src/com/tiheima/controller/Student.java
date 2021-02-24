5526f30aca08cc9cc32b965ae3e1eaf8a228e3b8
jimindong <15539277254@163.com>
2021-02-24 22:35:15 +0800

    提交Student1

--- git_test/src/com/tiheima/controller/Student.java
+++ git_test/src/com/tiheima/controller/Student.java
@@ -10,6 +10,7 @@ public class Student {
         System.out.println("0000");//23456789
         //09876543243567
         System.out.println("0000");
+		//98765456789098784323244
         System.out.println("234567890");
         System.out.println("0000");
         System.out.println("0000");
